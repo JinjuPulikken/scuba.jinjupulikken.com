b5edb5317397fab82ee1fc43522a4446e721ade1
JinjuPulikken <118257756+JinjuPulikken@users.noreply.github.com>
2023-06-21 10:36:55 +0400

    Update mail.php

--- mail.php
+++ mail.php
@@ -4,11 +4,11 @@
 $name = $_POST['name'];
 $email= $_POST['email'];
 $message= $_POST['message'];
-$to = "youremail@mail.com";
+$to = "jinju.pulikken@regenteducation.ae";
 $subject = "Mail From website";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
 $headers = "From: noreply@yoursite.com" . "\r\n" .
-"CC: somebodyelse@example.com";
+"CC: jinju.pulikken@regenteducation.ae";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
 }
